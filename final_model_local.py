fix and optimized model, remove drop out, change normalization

final_model_local.py
@@ -14,7 +14,7 @@ from torch_geometric.data import Data
 from torch_geometric.nn import TransformerConv, global_mean_pool
 
 from utils.data_processing import *
-from utils.architecture import GIN
+from utils.architecture import GIN, Transformer
 from utils.train_model import train_model
 from utils.evaluate_model import *
 
@@ -123,38 +123,6 @@ def load_dataset(data_dir, folder_list):
     return dataset
 
 
-class Transformer(torch.nn.Module):
-    def __init__(self, dim_h, node_feature, heads=4):
-        super().__init__()
-        self.conv1 = TransformerConv(node_feature, dim_h, heads=heads)
-        self.conv2 = TransformerConv(dim_h * heads, dim_h, heads=heads)
-        self.conv3 = TransformerConv(dim_h * heads, dim_h, heads=heads)
-
-        self.lin1 = Linear(dim_h * heads * 3, dim_h * 2)
-        self.lin2 = Linear(dim_h * 2, dim_h * 2)
-        self.lin3 = Linear(dim_h * 2, 1)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, edge_index, batch):
-        h1 = self.relu(self.conv1(x, edge_index))
-        h2 = self.relu(self.conv2(h1, edge_index))
-        h3 = self.relu(self.conv3(h2, edge_index))
-
-        h1 = global_mean_pool(h1, batch)
-        h2 = global_mean_pool(h2, batch)
-        h3 = global_mean_pool(h3, batch)
-
-        h = torch.cat((h1, h2, h3), dim=1)
-
-        h = self.relu(self.lin1(h))
-        h = F.dropout(h, p=0.5, training=self.training)
-        h = self.relu(self.lin2(h))
-        h = F.dropout(h, p=0.5, training=self.training)
-        h = self.lin3(h)
-
-        return h.squeeze()
-
-
 def plot_dataset(dataset, output_dir):
     E_vals = [data.E for data in dataset]
 
@@ -176,16 +144,7 @@ def plot_dataset(dataset, output_dir):
         x = range(start, end)
         y = E_vals[start:end]
 
-        plt.plot(
-            x,
-            y,
-            linestyle="none",
-            marker=markers[i],
-            color=colors[i],
-            markersize=3,
-            label=labels[i],
-            alpha=0.4,
-        )
+        plt.plot( x, y, linestyle="none", marker=markers[i], color=colors[i], markersize=3, label=labels[i], alpha=0.4)
 
     plt.ylabel(r"Elastic Modulus $E$ (GPa)")
     plt.xlabel("Samples")
@@ -235,7 +194,7 @@ def main():
 
     dataset = load_dataset(args.data_dir, folder_list)
 
-    duplicates = check_duplicate_graphs(dataset)
+    check_duplicate_graphs(dataset)
 
     batch_size = args.batch_num
     epoch_num = args.epoch_num
@@ -266,9 +225,7 @@ def main():
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
     criterion = nn.MSELoss()
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode="min", patience=5, factor=0.5, min_lr=1e-5
-    )
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau( optimizer, mode="min", patience=5, factor=0.5, min_lr=1e-5)
 
     if torch.backends.mps.is_available():
         device = torch.device("mps")
@@ -289,14 +246,8 @@ def main():
     print("Number of parameters:", num_params)
 
     train_losses, test_losses, R2_trainings, R2_tests, best_state_dict = train_model(
-        model,
-        train_loader,
-        test_loader,
-        criterion,
-        optimizer,
-        scheduler,
-        device=device,
-        num_epochs=epoch_num,
+        model, train_loader, test_loader, criterion, 
+        optimizer, scheduler, device=device, num_epochs=epoch_num,
     )
 
     plot_losses(train_losses, test_losses, epoch_num, args.save_model_dir)
@@ -305,14 +256,11 @@ def main():
     torch.save(best_state_dict, best_model_path)
     print(f"Saved best model to: {best_model_path}")
 
-    print("Training data:")
-    evaluate_model(model, train_loader, device, args.cover_interval, overlap, args.save_model_dir)
+    model.load_state_dict(best_state_dict)
 
-    print("Validation data:")
-    evaluate_model(model, test_loader, device, args.cover_interval, overlap, args.save_model_dir)
-
-    print("Testing data:")
-    evaluate_model(model, valid_loader, device, args.cover_interval, overlap, args.save_model_dir)
+    evaluate_model(model, train_loader, device, args.cover_interval, overlap, args.save_model_dir, split_name="train")
+    evaluate_model(model, test_loader, device, args.cover_interval, overlap, args.save_model_dir, split_name="validation")
+    evaluate_model(model, valid_loader, device, args.cover_interval, overlap, args.save_model_dir, split_name="test")
 
 
 if __name__ == "__main__":

utils/architecture.py
@@ -40,9 +40,9 @@ class GIN(torch.nn.Module):
         
         # Classifier
         h = self.relu(self.lin1(h))
-        h = F.dropout(h, p=0.5, training=self.training)
+        #h = F.dropout(h, p=0.0, training=self.training)
         h = self.relu(self.lin2(h))
-        h = F.dropout(h, p=0.5, training=self.training)
+        #h = F.dropout(h, p=0.0, training=self.training)
         h = self.lin4(h)
         
         return h.squeeze()
@@ -74,9 +74,9 @@ class Transformer(torch.nn.Module):
         h = torch.cat((h1, h2, h3), dim=1)
 
         h = self.relu(self.lin1(h))
-        h = F.dropout(h, p=0.5, training=self.training)
+        #h = F.dropout(h, p=0.0, training=self.training)
         h = self.relu(self.lin2(h))
-        h = F.dropout(h, p=0.5, training=self.training)
+        #h = F.dropout(h, p=0.0, training=self.training)
         h = self.lin3(h)
 
         return h.squeeze()

utils/data_processing.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset, random_split
 from itertools import permutations
 import networkx as nx
 from torch_geometric.data import Data
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 
     
@@ -39,8 +39,9 @@ def make_undirected(data):
     missing_edges = [(b, a) for a, b in edge_set if (b, a) not in edge_set]
 
     # Convert lists of missing edges to tensor format and add them to the original edge_index
-    missing_edges_tensor = torch.tensor(missing_edges, dtype=torch.long).t()
-    data.edge_index = torch.cat([edge_index, missing_edges_tensor], dim=1)
+    if len(missing_edges) > 0:
+        missing_edges_tensor = torch.tensor(missing_edges, dtype=torch.long).t()
+        data.edge_index = torch.cat([edge_index, missing_edges_tensor], dim=1)
     
     return data
 
@@ -88,9 +89,12 @@ def normalize_planar_info(data_list):
     for data in data_list:
         num_features = data.x.shape[1]
         for i in range(0, num_features):
+
+            min_val = torch.min(data.x[:, i]).item()
             max_val = torch.max(data.x[:, i]).item()
-            if max_val != 0:
-                data.x[:, i] /= max_val
+            
+            if max_val - min_val != 0:
+                data.x[:, i] = (data.x[:, i] - min_val) / (max_val - min_val)
 
     return data_list
 

utils/train_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from sklearn.metrics import r2_score
+import copy
 
 def train_model(model, train_loader, test_loader, criterion, optimizer, scheduler, device='cuda', num_epochs=1):
     model.to(device)
@@ -13,8 +14,8 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, schedule
     best_state_dict = None
     best_loss = float('inf')
     
-    model.train()
     for epoch in range(num_epochs):
+        model.train()
         train_loss = 0
         total_graphs = 0
         pred_whole = []
@@ -79,7 +80,7 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, schedule
         
         if test_loss < best_loss:
             best_loss = test_loss
-            best_state_dict = model.state_dict()            
+            best_state_dict = copy.deepcopy(model.state_dict())
         print(f'Epoch [{epoch+1}], LR [{current_lr}], Loss[Train: {epoch_loss:.3f}, Test: {test_loss:.3f}], R2[Train: {R2_train:.3f}, Test: {R2_test:.3f}]')
     
     print(f'Final Test Loss: {test_losses[-1]:.4f}')